cb290919fb6a41f542ded793c553240b88f8cb17
Luis <luisruben2997@gmail.com>
2022-12-14 22:39:14 -0600

    si

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivy.lang.builder import Builder
 from kivymd.app import MDApp
 from kivy.core.window import Window
 import operaciones
+from kivy.uix.image import Image
 
 Window.size=(800,250)
 
@@ -9,11 +10,10 @@ class CalculadoraVenn(MDApp):
     def build(self):
         return Builder.load_file('main.kv')
 
-    def backspace(self):
-        text=self.root.ids.operation.text
-        n=len(text)
-        self.root.ids.operation.text=text[0:n-2]
-    
+    def delete(self):
+        self.root.ids.operation.text=''
+        self.root.ids.resultado.clear_widgets()
+
     def calculate(self):
         text=self.root.ids.operation.text
         text=text.strip()
@@ -23,7 +23,6 @@ class CalculadoraVenn(MDApp):
             operaciones._3sets(text)
         else:
             print('Tas mal chavo')
-        self.root.ids.resultado.source=''
         self.root.ids.resultado.source='graph.png'
 
 if __name__=='__main__':
--- operaciones.py
+++ operaciones.py
@@ -1,8 +1,10 @@
 from matplotlib import pyplot as plt
 import numpy as np
 from matplotlib_venn import venn3,venn2
+from kivy.cache import Cache
 
 def _2sets(operation):
+    plt.clf()
     operation=operation.split(' ')
     sets={}
     sets['A']={'100','110'}
@@ -17,6 +19,9 @@ def _2sets(operation):
         result=set(set1)|set(set2)
     elif operation[1]=='n':
         result=set(set1).intersection(set2)
+    elif operation[1]=='Δ':
+        result=set(set1).symmetric_difference(set2)
+
 
     diagrama=venn2(subsets=(1, 1, 1),set_labels=('A','B'),alpha=1)
     for i in ('100','110','010'):
@@ -29,10 +34,13 @@ def _2sets(operation):
     for i in ('10','11','01'):
         diagrama.get_patch_by_id(i).set_edgecolor("black")
     
+    Cache.remove("kv.image", 'graph.png' + "|False|0")
+    Cache.remove("kv.texture", 'graph.png' + "|False|0")
     plt.savefig('graph.png')
     return result
 
 def _3sets(operation):
+    global i
     operation=operation.split(' ')
 
     set3=['111','011','001','101']
@@ -51,6 +59,9 @@ def _3sets(operation):
         result=set(res_1op)|set(set3)
     elif operation[3]=='n':
         result=set(res_1op).intersection(set3)
+    elif operation[3]=='Δ':
+        result=set(res_1op).symmetric_difference(set3)
+
 
     diagrama=venn3(subsets=(1, 1, 1,1,1,1,1),set_labels=('A','B','C'),alpha=1)
     for i in ('100','110','010','001','101','111','011'):
@@ -63,4 +74,5 @@ def _3sets(operation):
     for i in ('100','110','010','001','101','111','011'):
         diagrama.get_patch_by_id(i).set_edgecolor("black")
     
+    Cache.remove('kv.image')
     plt.savefig('graph.png')
